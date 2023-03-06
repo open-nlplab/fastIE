29e3643e160c9464727b05e90f478e277b9f75dd
00INDEX <zhangshuolove@live.com>
2023-03-06 17:08:35 +0800

    fix bug: load state_dict to cpu first

--- fastie/utils/hub.py
+++ fastie/utils/hub.py
@@ -1,6 +1,14 @@
 import os
+import io
 import pickle
 
+class Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            import torch
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
 
 class Hub:
 
@@ -8,7 +16,7 @@ class Hub:
     def load(cls, path: str):
         if os.path.exists(path) and os.path.isfile(path):
             with open(path, mode='br') as file:
-                return pickle.load(file)
+                return Unpickler(file).load()
         else:
             # 到 s3 上下载
             pass
